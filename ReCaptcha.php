f6071f3f637f5bd85e067811508cd415ad45b6d5
quentinfslr <quentin.fslr@eduge.ch>
2018-07-06 19:23:42 +0200

    Modif/Add Key

--- ReCaptcha.php
+++ ReCaptcha.php
@@ -1,11 +1,17 @@
 <?php
-$KeyGoogle = "";
+//Cles 
+$KeySite = "";
+$KeySecret ="";
 
 function ReCaptcha() {
     global $KeyGoogle;
     ?>
+ <script src = "https://www.google.com/recaptcha/api.js" async defer ></script>
     <div class = "g-recaptcha" data-sitekey ="<?=$KeyGoogle?>" >
     </div> <br/> 
     <?php
+}
+function ReCaptchaValider(){
+    
 }
 ?>
