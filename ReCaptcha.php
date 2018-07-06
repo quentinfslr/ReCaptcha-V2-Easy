a70d4fb8a331a81c98e6b15605ea074ae655fe0b
quentinfslr <quentin.fslr@eduge.ch>
2018-07-06 19:37:43 +0200

    Add function ReCaptchaValider

--- ReCaptcha.php
+++ ReCaptcha.php
@@ -1,17 +1,34 @@
 <?php
 //Cles 
 $KeySite = "";
-$KeySecret ="";
+$KeySecret = "";
 
 function ReCaptcha() {
     global $KeySite;
     ?>
- <script src = "https://www.google.com/recaptcha/api.js" async defer ></script>
-    <div class = "g-recaptcha" data-sitekey ="<?=$KeySite?>" >
+    <script src = "https://www.google.com/recaptcha/api.js" async defer ></script>
+    <div class = "g-recaptcha" data-sitekey ="<?= $KeySite ?>" >
     </div> <br/> 
     <?php
 }
-function ReCaptchaValider(){
+
+function ReCaptchaValider() {
+    global $KeySecret;
+
+    //Parameter returned by the recaptcha
+    $Reply = $_POST['g-recaptcha-response'];
+    //We recover the IP
+    $remoteip = $_SERVER['REMOTE_ADDR'];
     
+
+    $api_url = "https://www.google.com/recaptcha/api/siteverify?secret=".$KeySecret. "&response=".$Reply."&remoteip=".$remoteip;
+    $decode = json_decode(file_get_contents($api_url), true);
+
+    //Check Reply
+    if ($decode['success'] == true)
+        return true;
+     else 
+        return false;
+ 
 }
 ?>
